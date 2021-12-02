35c0884aa007897b257594f985889af10dd956ab
Erfan <erfan.samieyan95@gmail.com>
2021-12-02 12:13:09 +0000

    Adding gallery and room for images

--- app/src/main/java/com/triptracker/activities/MapsActivity.kt
+++ app/src/main/java/com/triptracker/activities/MapsActivity.kt
@@ -1,7 +1,10 @@
-package com.example.androiddevproj
+package com.triptracker.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -9,7 +12,8 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import com.example.androiddevproj.databinding.ActivityMapsBinding
+import com.triptracker.R
+import com.triptracker.databinding.ActivityMapsBinding
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -26,6 +30,11 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        findViewById<Button>(R.id.gallery_btn).setOnClickListener(View.OnClickListener {
+            val intent = Intent(this, GalleryActivity::class.java)
+            this.startActivity(intent)
+        })
     }
 
     /**
